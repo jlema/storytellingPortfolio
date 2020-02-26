d99be2b873bccbd69be38ef082e8424c4719fbfe
John (Juan) Lema <juan@juanlema.com>
2020-02-26 15:29:49 -0800

    Added responsive (column number) card stacking Replaced hardcoded values with bootstrap functions in SCSS Started to add topic filtering interaction

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
-$(function() {
+$(function () {
   // Sidebar toggle behavior
   // From: https://bootstrapious.com/p/bootstrap-vertical-navbar
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar, #content').toggleClass('active');
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar, #content").toggleClass("active");
   });
 });
+
+let cardsAll = ['card_aadv', 'card_artfullyOpera', 'card_behindOpera', 'card_bouchra', 'card_capitalOpera',
+  'card_ecotraveller', 'card_gadventures', 'card_highPakistan', 'card_jordanBeauty',
+  'card_jordanTrail', 'card_katyaKabanova', 'card_planeterra', 'card_shadowAni',
+  'card_warOnWalls'];
+
+document
+  .getElementById("topics_overview")
+  .addEventListener("click", function () {
+    cardsAll.forEach(function (item, index) {
+      document.getElementById(item).style.display = "inline-block";
+    });
+
+    // Needs all topics to be de-highlighted
+    // document.getElementById(item).classList.remove('bg-light');
+
+  });
+
+document
+  .getElementById("topics_seattleOpera")
+  .addEventListener("click", function () {
+    document.getElementById("card_aadv").style.display = 'none';
+  });
